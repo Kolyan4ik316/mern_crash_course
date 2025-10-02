052b765726cb296b94a5df2991cfdf7359512ffb
Kolyan4ik316 <Kolyan4ikn1@gmail.com>
2025-10-03 02:00:43 +0300

    my first commit. Done with simple http requests and connected to mongodb

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import {connectDB} from './config/db.js'
+import productRoutes from './routes/product.route.js'
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())  //allow to accept json data in body
+
+app.use("/api/products", productRoutes)
+
+app.listen(5000, () => {
+    connectDB()
+    console.log('Server is running on port 5000')
+})
